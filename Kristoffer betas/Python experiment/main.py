9ec8acff87a6525ddbe88b3cafa94ea285204ff7
kvkunkel <kristoffervkunkel@gmail.com>
2016-02-18 14:40:20 +0100

    TF-IDF Indexer

--- Kristoffer betas/Python experiment/main.py
+++ Kristoffer betas/Python experiment/main.py
@@ -6,4 +6,10 @@ xmlpath = path + "/XML parser/xmlparser.py"
 
 xmlpath = xmlpath.replace(" ", "*")
 
+print "running: %s" % xmlpath
 os.system("python " + xmlpath)
+
+tfidfpath = (path + "/TF-IDF indexer/tediffern.py").replace(" ", "*")
+
+print "running: %s" % tfidfpath
+os.system("python " + tfidfpath)
