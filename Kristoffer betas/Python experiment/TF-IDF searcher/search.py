47cd06ca65db0305f2fd7f7a95259594b2f090fa
kvkunkel <kristoffervkunkel@gmail.com>
2016-03-02 15:15:34 +0100

    Added 'input from file' feature, to help testing

--- Kristoffer betas/Python experiment/TF-IDF searcher/search.py
+++ Kristoffer betas/Python experiment/TF-IDF searcher/search.py
@@ -6,6 +6,7 @@ import pickle
 
 invertedindexpath = os.getcwd() + "/TF-IDF indexer/inverted_index.in"
 articleindexpath = os.getcwd() + "/TF-IDF indexer/articlecounts.in"
+searchtermspath = os.getcwd() + "/TF-IDF searcher/searchterms.txt"
 
 # invertedindexpath = os.getcwd() + "/inverted_index.txt"
 # articleindexpath = os.getcwd() + "/articlecounts.txt"
@@ -26,26 +27,38 @@ tempIndex.close()
 
 totaldoccount = len(articleindex)
 
-term = str(raw_input("term? "))
+searchterms = open(searchtermspath, "r")
 
-tmpresult = []
+results = []
 
-if hash(term) in invertedindex:
-	articlehits = invertedindex[hash(term)][1]
-	doccount = len(articlehits)
-	IDF = math.log10(totaldoccount / float(doccount))
+for term in searchterms:
+	term = str(term).strip()
+	tmpresult = []
+	if hash(term) in invertedindex:
+		articlehits = invertedindex[hash(term)][1]
+		doccount = len(articlehits)
+		IDF = math.log10(totaldoccount / float(doccount))
 
-	for article in articlehits:
-		wordcount = articlehits[article]
-		articlewordcount = articleindex[article]
-		TF = wordcount / float(articlewordcount)
-		TFIDF = TF * IDF
-		tmpresult.append((article,TFIDF))
-		print "TFIDF: %s * %s = %s" % (TF, IDF, TFIDF)
+		for article in articlehits:
+			wordcount = articlehits[article]
+			articlewordcount = articleindex[article]
+			TF = wordcount / float(articlewordcount)
+			TFIDF = TF * IDF
+			tmpresult.append((article,TFIDF))
+			# print "TFIDF: %s * %s = %s" % (TF, IDF, TFIDF)
+	else:
+		print "The term '%s' does not exist in the corpus." %term
+	if len(tmpresult) < 1:
+		continue
+	# Sort results on their TFIDF rating, in decreasing order.
+	tmpresult = sorted(tmpresult, key=lambda result: result[1], reverse=True)
+	# Deletes the bottom 50% of our search results
+	tmpresult = tmpresult[0:len(tmpresult)/2]
+	if len(tmpresult) > 0:
+		results.append((term,tmpresult))
 
-# Sort results on their TFIDF rating, in decreasing order.
-results = sorted(tmpresult, key=lambda result: result[1], reverse=True)
 
-# Print the top 50% of our results
-for result in results[0:len(results)/2]:
-	print result
+for result in results:
+	print result[0]
+	for articles in result[1]:
+		print articles
