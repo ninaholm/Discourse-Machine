cf10624b317214285ac15b8a5dad52a0cca0e3fa
kvkunkel <kristoffervkunkel@gmail.com>
2016-05-12 15:41:00 +0200

    Setup POSCorpus and SyntacticParser to work from main. Need to edit SyntacticParser to handle POSCorpus sentences [[word, tag]...].

--- Pipeline/POSCorpus/POSCorpus.py
+++ Pipeline/POSCorpus/POSCorpus.py
@@ -15,22 +15,27 @@
 # 		Reads through all articles in the subsetlist and returns every sentence with the term in it.
 
 # 		returns [sentences]
+from SyntacticParser.SyntacticParser import *
 import os
 import pickle
+import csv
+
 
 class POSCorpus():
 	def __init__(self, inputfiles, subsetList):
 		self.articleDict = {}
 		self.inputfiles = inputfiles
-		self.subsetList = []
+		self.subsetList = subsetList
 		self.scores = []
+		self.sentimentdict = self.getSentimentDict()
 
 	def load(self):
 		inputpath = os.getcwd() + "/data/postagger_output/"
 
 		joinedSubset = {}
-		for tuple in self.subsetList:
-			for articleid in tuple[1]:
+		for subset_term in self.subsetList:
+			for articleid in subset_term[1]:
+				# print "articleid: ", articleid
 				joinedSubset[articleid] = True
 
 		for inputfilename in self.inputfiles:
@@ -41,30 +46,41 @@ class POSCorpus():
 			for x in tmp:
 				# print "0: ", tmp[x][0]
 				# print "lol: ", tmp[x][4]
-				self.articleDict[x] = tmp[x]
-				# if x in joinedSubset:
-				# 	articleDict[x] = tmp[x]
+				# self.articleDict[x] = tmp[x]
+				if x in joinedSubset:
+					self.articleDict[x] = tmp[x]
 			pickledData.close()
 
-	def score_sentiment(self, subsetList):
-		term, subset = subsetList
+	def score_sentiment(self, term_subset):
+		term, subset = term_subset
+		print ">>SENTIMENTSCORE: Scoring sentiment for '%s'." % term
 		sentences = []
 		sentimentscore = 0
 		
 		for articleid in subset:
-			# article = self.articleDict[articleid]
-			article = self.articleDict.popitem()
+			article = self.articleDict[articleid]
+			tmp = self.get_sentences(article, term)
+			for x in tmp:
+				sentences.append(x)
+			break
 
-			sentences.append(self.get_sentences(article))
+		print ">>SENTIMENTSCORE: Found %s sentences."%len(sentences)
 
-		# for sentence in sentences:
-		# 	sentimentscore += syntacticParser(sentence)
+		parser = SyntacticParser()
 
-		# self.scores.append((term,sentimentscore))
+		for sentence in sentences:
+			t = parser.parse_sentence(sentence)
 
-	def get_sentences(self, article):
+			if t is not None:
+				print t.tree
+				sentimentscore += t.get_sentiment_score(self.sentimentdict, term)
+
+		self.scores.append((term,sentimentscore))
+
+	def get_sentences(self, article, term):
 		sentenceList = []
-		for entry in article[1][1:]:
+
+		for entry in article[1:]:
 			# print entry, len(repr(entry))
 			if len(repr(entry)) < 7:
 				continue
@@ -72,6 +88,9 @@ class POSCorpus():
 			entry = entry.split("./TEGN")
 
 			for sentences in entry:
+				sentimenthit = False
+				termhit = False
+
 				sentence = []
 				words = sentences.split(" ")
 
@@ -82,9 +101,34 @@ class POSCorpus():
 						continue
 					if len(word) < 1: 
 						continue
-					sentence.append(word)
+					if word[:word.rfind("/")] in self.sentimentdict:
+						# print ">> SENTIMENTHIT", word[:word.rfind("/")], " = ", self.sentimentdict[word[:word.rfind("/")]]
+						sentimenthit = True
+					if word[:word.rfind("/")] == term:
+						# print ">> TERMHIT", term
+						termhit = True
+					sentence.append(word.split("/"))
+					# print word[:word.rfind("/")]
 					# print "word: %s" %repr(word)
-				print sentence
-				sentenceList.append(sentence)
+				# print "--" * 20
+				if sentimenthit and termhit:
+					sentenceList.append(sentence)
 
 		return sentenceList
+
+	def print_sentence(self, sentence):
+		sentencestring = ""
+		for x in sentence:
+			sentencestring += x[0] + " "
+		print sentencestring
+
+
+	def getSentimentDict(self):
+		dict_path = "Corpus/sentiment_dictionaries/universal_dictionary.csv"
+		with open(dict_path, "r") as csvfile:
+			dictionary = {}
+			with open(dict_path, "r") as csvfile:
+				csv_dict = csv.reader(csvfile)
+				for row in csv_dict:
+					dictionary[row[0].decode('utf-8')] = row[1]
+		return dictionary
